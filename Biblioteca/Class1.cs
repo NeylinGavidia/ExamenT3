c3a89a365fd7ae952119694030f9f7d8c721ea0b
Neylin <n00199817@upn.pe>
2025-06-13 09:28:27 -0500

    Agregar archivos de proyecto.

---
+++ Biblioteca/Class1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca
+{
+    public class Class1
+    {
+        private Random random = new Random();
+
+        public int[] GenerarNumeros(int cantidad)
+        {
+            int[] numeros = new int[cantidad];
+            for (int i = 0; i < cantidad; i++)
+            {
+                numeros[i] = random.Next(1, 100);
+            }
+            return numeros;
+        }
+        public void Mostrarnumeros(int[] numeros)
+        {
+            Console.WriteLine(string.Join(",", numeros));
+        }
+
+        //Primera opcion
+        public void MostrarNumerosAlCubo(int[] arreglo)
+        {
+            Console.WriteLine("Elementos elevados a la tercera potencia:");
+            for (int i = 0; i < arreglo.Length; i++)
+            {
+                Console.Write(Math.Pow(arreglo[i], 3) + " ");
+            }
+            Console.WriteLine();
+        }
+        //segunda opcion
+        public void Burbuja(int[] numeros)
+        {
+            int num = numeros.Length;
+
+            for (int i = 0; i < num - 1; i++)
+            {
+                for (int j = 0; j < num - i - 1; j++)
+                {
+                    if (numeros[j] < numeros[j + 1])
+                    {
+                        // Intercambiar correctamente
+                        int aux = numeros[j];
+                        numeros[j] = numeros[j + 1];
+                        numeros[j + 1] = aux;
+                    }
+                }
+            }
+        }
+        //tercera opcion
+        public int[] Eliminar(int[] eliminado, int indice)
+        {
+            if (indice < 0 || indice >= eliminado.Length)
+            {
+                Console.WriteLine("Opción no válida.Índice no existente");
+                Console.WriteLine();
+                return eliminado;
+            }
+
+            int[] gruponuevo = new int[eliminado.Length - 1];
+            int j = 0;
+
+            for (int i = 0; i < eliminado.Length; i++)
+            {
+                if (i == indice) continue; // continue, investigado//para saltar el indice eliminado y que no se digite
+                gruponuevo[j] = eliminado[i];
+                j++;
+            }
+
+            Console.WriteLine("Elemento eliminado. Los números son:");
+            Console.WriteLine();
+            Mostrarnumeros(gruponuevo);
+
+            return gruponuevo;
+        }
+        //cuarta opncion
+        public void ModificarPorValor(int[] arreglo, int valorActual, int nuevoValor)
+        {
+            bool encontrado = false;
+
+            for (int i = 0; i < arreglo.Length; i++)
+            {
+                if (arreglo[i] == valorActual)
+                {
+                    arreglo[i] = nuevoValor;
+                    encontrado = true;
+                }
+            }
+
+            if (encontrado)
+            {
+                Console.WriteLine("Elemento modificado. Nueva lista:");
+                Mostrarnumeros(arreglo);
+            }
+            else
+            {
+                Console.WriteLine("Valor no encontrado en el arreglo.");
+            }
+        }
+    }
+}
---
+++ ExamenT3/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using Biblioteca;
+
+namespace ExamenT3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int op;
+            Class1 acciones = new Class1();
+
+            Console.WriteLine("Hola, ¿Cuántos números deseas generar?");
+            Console.WriteLine();
+            int cantidad = int.Parse(Console.ReadLine());
+            int[] numeros = acciones.GenerarNumeros(cantidad);
+            Console.WriteLine();
+
+            Console.WriteLine("Los números generados son: ");
+            Console.WriteLine();
+            acciones.Mostrarnumeros(numeros);
+            Console.WriteLine();
+            Console.WriteLine("Gracias por generar los números, presione ENTER para continuar");
+            Console.ReadKey();
+            Console.Clear();
+
+            do
+            {
+                Console.WriteLine("Bienvendx al menú");
+                Console.WriteLine();
+                Console.WriteLine("1. Tercera potencia");
+                Console.WriteLine("2. Ordenar de manera descendente");
+                Console.WriteLine("3. Eliminar elemento");
+                Console.WriteLine("4. Modificar elemento");
+                Console.WriteLine("0. Salir");
+                Console.WriteLine();
+                op = int.Parse(Console.ReadLine());
+                Console.Clear();
+                Console.WriteLine();
+                switch (op)
+                {
+                    case 1:
+                        acciones.MostrarNumerosAlCubo(numeros);
+                        Thread.Sleep(2000);
+                        Console.Clear();
+                        break;
+                    case 2:
+                        Console.WriteLine("Ordenando...");
+                        Thread.Sleep(500);
+                        Console.Clear();
+                        Console.WriteLine("Orden descendente:");
+                        Console.WriteLine();
+                        acciones.Burbuja(numeros);
+                        acciones.Mostrarnumeros(numeros);
+                        Thread.Sleep(2000);
+                        Console.Clear();
+                        break;
+                    case 3:
+                        Console.WriteLine("¿Qué posición deseas eliminar?");
+                        Console.WriteLine();
+                        int indice = int.Parse(Console.ReadLine());
+                        Console.WriteLine();
+                        numeros = acciones.Eliminar(numeros, indice);
+                        Thread.Sleep(2000);
+                        Console.Clear();
+                        break;
+                    case 4:
+                        Console.WriteLine("¿Qué número deseas modificar?");
+                        Console.WriteLine();
+                        int actual = int.Parse(Console.ReadLine());
+                        Console.WriteLine();
+
+                        Console.WriteLine("¿Por qué número lo deseas reemplazar?");
+                        Console.WriteLine();
+                        int nuevo = int.Parse(Console.ReadLine());
+                        Console.WriteLine();
+
+                        acciones.ModificarPorValor(numeros, actual, nuevo);
+                        Thread.Sleep(2500);
+                        Console.Clear();
+                        break;
+                    case 0:
+                        Console.Clear();
+                        return;
+                    default:
+                        Console.WriteLine("Opción no válida");
+                        Console.WriteLine();
+                        break;
+                }
+            }
+            while (true);
+        }
+    }
+}
